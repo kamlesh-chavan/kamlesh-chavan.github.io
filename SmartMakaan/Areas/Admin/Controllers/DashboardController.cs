0dcf86b1da4ca123f9d8ab4fea04937d6daf4c71
kamleshchavan007 <kamleshchavan007@gmail.com>
2018-11-17 11:17:50 +0530

    Initial commit

---
+++ SmartMakaan/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartMakaan.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Home
+        [Route("Dashboard")]
+        public ActionResult Index()
+        {
+            return View();
+        }       
+    }
+}
---
+++ SmartMakaan/Areas/Admin/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartMakaan.Areas.Admin.Models
+{
+    public class City
+    {
+        [Required(ErrorMessage = "City name is required")]
+        public string cityName { get; set; }
+        public string cityDescription { get; set; }
+        public string cityImage { get; set; }
+    }
+}
